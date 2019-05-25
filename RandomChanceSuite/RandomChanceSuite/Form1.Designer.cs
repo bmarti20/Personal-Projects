f477b3eae47aa40406aad94801713c621e05a2c2
Benjamin Martin <bmartin6245@yahoo.com>
2019-05-25 01:55:54 -0600

    Added Die Roll. Allows user to roll a 6 sided die. User can roll once or a set number of times using textbox. All outcomes are tracked.

--- RandomChanceSuite/RandomChanceSuite/Form1.Designer.cs
+++ RandomChanceSuite/RandomChanceSuite/Form1.Designer.cs
@@ -29,45 +29,47 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CoinPictureBox = new System.Windows.Forms.PictureBox();
             this.CoinFlipButton = new System.Windows.Forms.Button();
             this.HeadsOutcomesLabel = new System.Windows.Forms.Label();
             this.TailsOutcomesLabel = new System.Windows.Forms.Label();
             this.MultipleCoinFlipButton = new System.Windows.Forms.Button();
             this.FlipXTimesTextbox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.DiePictureBox = new System.Windows.Forms.PictureBox();
             this.RollDieButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.RollOutcomesLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MultipleDieRollButton = new System.Windows.Forms.Button();
+            this.RollXTimesTextbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CoinPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiePictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(16, 10);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(26, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 36);
+            this.label1.Size = new System.Drawing.Size(110, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Coin Flip";
             // 
-            // pictureBox1
+            // CoinPictureBox
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(23, 49);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(88, 79);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.CoinPictureBox.Location = new System.Drawing.Point(17, 40);
+            this.CoinPictureBox.Name = "CoinPictureBox";
+            this.CoinPictureBox.Size = new System.Drawing.Size(128, 128);
+            this.CoinPictureBox.TabIndex = 1;
+            this.CoinPictureBox.TabStop = false;
             // 
             // CoinFlipButton
             // 
-            this.CoinFlipButton.Location = new System.Drawing.Point(16, 135);
-            this.CoinFlipButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CoinFlipButton.Location = new System.Drawing.Point(17, 172);
             this.CoinFlipButton.Name = "CoinFlipButton";
-            this.CoinFlipButton.Size = new System.Drawing.Size(100, 28);
+            this.CoinFlipButton.Size = new System.Drawing.Size(75, 23);
             this.CoinFlipButton.TabIndex = 2;
             this.CoinFlipButton.Text = "Flip Once";
             this.CoinFlipButton.UseVisualStyleBackColor = true;
@@ -76,29 +78,28 @@
             // HeadsOutcomesLabel
             // 
             this.HeadsOutcomesLabel.AutoSize = true;
-            this.HeadsOutcomesLabel.Location = new System.Drawing.Point(119, 60);
-            this.HeadsOutcomesLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.HeadsOutcomesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeadsOutcomesLabel.Location = new System.Drawing.Point(151, 69);
             this.HeadsOutcomesLabel.Name = "HeadsOutcomesLabel";
-            this.HeadsOutcomesLabel.Size = new System.Drawing.Size(57, 17);
+            this.HeadsOutcomesLabel.Size = new System.Drawing.Size(49, 15);
             this.HeadsOutcomesLabel.TabIndex = 4;
             this.HeadsOutcomesLabel.Text = "Heads: ";
             // 
             // TailsOutcomesLabel
             // 
             this.TailsOutcomesLabel.AutoSize = true;
-            this.TailsOutcomesLabel.Location = new System.Drawing.Point(119, 97);
-            this.TailsOutcomesLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TailsOutcomesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TailsOutcomesLabel.Location = new System.Drawing.Point(151, 111);
             this.TailsOutcomesLabel.Name = "TailsOutcomesLabel";
-            this.TailsOutcomesLabel.Size = new System.Drawing.Size(46, 17);
+            this.TailsOutcomesLabel.Size = new System.Drawing.Size(39, 15);
             this.TailsOutcomesLabel.TabIndex = 5;
             this.TailsOutcomesLabel.Text = "Tails: ";
             // 
             // MultipleCoinFlipButton
             // 
-            this.MultipleCoinFlipButton.Location = new System.Drawing.Point(16, 171);
-            this.MultipleCoinFlipButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MultipleCoinFlipButton.Location = new System.Drawing.Point(17, 197);
             this.MultipleCoinFlipButton.Name = "MultipleCoinFlipButton";
-            this.MultipleCoinFlipButton.Size = new System.Drawing.Size(100, 28);
+            this.MultipleCoinFlipButton.Size = new System.Drawing.Size(75, 23);
             this.MultipleCoinFlipButton.TabIndex = 6;
             this.MultipleCoinFlipButton.Text = "Flip X Times:";
             this.MultipleCoinFlipButton.UseVisualStyleBackColor = true;
@@ -106,60 +107,114 @@
             // 
             // FlipXTimesTextbox
             // 
-            this.FlipXTimesTextbox.Location = new System.Drawing.Point(123, 174);
-            this.FlipXTimesTextbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FlipXTimesTextbox.Location = new System.Drawing.Point(98, 200);
             this.FlipXTimesTextbox.Name = "FlipXTimesTextbox";
-            this.FlipXTimesTextbox.Size = new System.Drawing.Size(51, 22);
+            this.FlipXTimesTextbox.Size = new System.Drawing.Size(47, 20);
             this.FlipXTimesTextbox.TabIndex = 7;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(245, 10);
+            this.label2.Location = new System.Drawing.Point(252, 8);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(120, 36);
+            this.label2.Size = new System.Drawing.Size(99, 29);
             this.label2.TabIndex = 8;
             this.label2.Text = "Die Roll";
             // 
-            // pictureBox2
+            // DiePictureBox
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(251, 49);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(101, 94);
-            this.pictureBox2.TabIndex = 9;
-            this.pictureBox2.TabStop = false;
+            this.DiePictureBox.Location = new System.Drawing.Point(236, 40);
+            this.DiePictureBox.Margin = new System.Windows.Forms.Padding(2);
+            this.DiePictureBox.Name = "DiePictureBox";
+            this.DiePictureBox.Size = new System.Drawing.Size(128, 128);
+            this.DiePictureBox.TabIndex = 9;
+            this.DiePictureBox.TabStop = false;
             // 
             // RollDieButton
             // 
-            this.RollDieButton.Location = new System.Drawing.Point(262, 149);
+            this.RollDieButton.Location = new System.Drawing.Point(236, 172);
+            this.RollDieButton.Margin = new System.Windows.Forms.Padding(2);
             this.RollDieButton.Name = "RollDieButton";
-            this.RollDieButton.Size = new System.Drawing.Size(75, 23);
+            this.RollDieButton.Size = new System.Drawing.Size(75, 21);
             this.RollDieButton.TabIndex = 10;
             this.RollDieButton.Text = "Roll";
             this.RollDieButton.UseVisualStyleBackColor = true;
+            this.RollDieButton.Click += new System.EventHandler(this.RollDieButton_Click);
+            // 
+            // RollOutcomesLabel
+            // 
+            this.RollOutcomesLabel.AutoSize = true;
+            this.RollOutcomesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RollOutcomesLabel.Location = new System.Drawing.Point(369, 69);
+            this.RollOutcomesLabel.Name = "RollOutcomesLabel";
+            this.RollOutcomesLabel.Size = new System.Drawing.Size(20, 90);
+            this.RollOutcomesLabel.TabIndex = 11;
+            this.RollOutcomesLabel.Text = "1: \r\n2: \r\n3: \r\n4: \r\n5: \r\n6: \r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(369, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Outcomes:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(151, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Outcomes:";
+            // 
+            // MultipleDieRollButton
+            // 
+            this.MultipleDieRollButton.Location = new System.Drawing.Point(236, 196);
+            this.MultipleDieRollButton.Margin = new System.Windows.Forms.Padding(2);
+            this.MultipleDieRollButton.Name = "MultipleDieRollButton";
+            this.MultipleDieRollButton.Size = new System.Drawing.Size(75, 22);
+            this.MultipleDieRollButton.TabIndex = 14;
+            this.MultipleDieRollButton.Text = "Roll X Times";
+            this.MultipleDieRollButton.UseVisualStyleBackColor = true;
+            this.MultipleDieRollButton.Click += new System.EventHandler(this.MultipleDieRollButton_Click);
+            // 
+            // RollXTimesTextbox
+            // 
+            this.RollXTimesTextbox.Location = new System.Drawing.Point(316, 198);
+            this.RollXTimesTextbox.Name = "RollXTimesTextbox";
+            this.RollXTimesTextbox.Size = new System.Drawing.Size(47, 20);
+            this.RollXTimesTextbox.TabIndex = 15;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(723, 217);
+            this.ClientSize = new System.Drawing.Size(471, 234);
+            this.Controls.Add(this.RollXTimesTextbox);
+            this.Controls.Add(this.MultipleDieRollButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.RollOutcomesLabel);
             this.Controls.Add(this.RollDieButton);
-            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.DiePictureBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.FlipXTimesTextbox);
             this.Controls.Add(this.MultipleCoinFlipButton);
             this.Controls.Add(this.TailsOutcomesLabel);
             this.Controls.Add(this.HeadsOutcomesLabel);
             this.Controls.Add(this.CoinFlipButton);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CoinPictureBox);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Random Chance Suite";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CoinPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiePictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,15 +223,20 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox CoinPictureBox;
         private System.Windows.Forms.Button CoinFlipButton;
         private System.Windows.Forms.Label HeadsOutcomesLabel;
         private System.Windows.Forms.Label TailsOutcomesLabel;
         private System.Windows.Forms.Button MultipleCoinFlipButton;
         private System.Windows.Forms.TextBox FlipXTimesTextbox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox DiePictureBox;
         private System.Windows.Forms.Button RollDieButton;
+        private System.Windows.Forms.Label RollOutcomesLabel;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button MultipleDieRollButton;
+        private System.Windows.Forms.TextBox RollXTimesTextbox;
     }
 }
 
--- RandomChanceSuite/RandomChanceSuite/Form1.cs
+++ RandomChanceSuite/RandomChanceSuite/Form1.cs
@@ -18,12 +18,23 @@ namespace RandomChanceSuite
         }
 
         public Image coin;
+        public Image die = Image.FromFile("die-faces.png");
         public static int numheads = 0;
         public static int numtails = 0;
+        public static int[] dierolls = { 0, 0, 0, 0, 0, 0 };
+
+        // Coin Flip Methods
 
         private void CoinFlipButton_Click(object sender, EventArgs e)
         {
             FlipCoin();
+        }
+
+        private void MultipleCoinFlipButton_Click(object sender, EventArgs e)
+        {
+            int.TryParse(FlipXTimesTextbox.Text, out int numflips);
+            for (int i = 0; i < numflips; i++)
+                FlipCoin();
         }
 
         private void FlipCoin()
@@ -43,22 +54,70 @@ namespace RandomChanceSuite
                     break;
             }
 
-            pictureBox1.Image = coin;
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            CoinPictureBox.Image = coin;
+            CoinPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
         }
 
-        private void MultipleCoinFlipButton_Click(object sender, EventArgs e)
+        // Die Roll Methods
+
+        private static Image cropImage(Image img, Rectangle cropArea)
         {
-            int.TryParse(FlipXTimesTextbox.Text, out int numflips);
-
-            for (int i = 0; i < numflips; i++)
-                FlipCoin();
-
+            Bitmap bmpImage = new Bitmap(img);
+            Bitmap bmpCrop = bmpImage.Clone(cropArea,
+            bmpImage.PixelFormat);
+            return (Image)(bmpCrop);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void RollDieButton_Click(object sender, EventArgs e)
         {
+            RollDie();
+        }
 
+        private void MultipleDieRollButton_Click(object sender, EventArgs e)
+        {
+            int.TryParse(RollXTimesTextbox.Text, out int numrolls);
+            for (int i = 0; i < numrolls; i++)
+                RollDie();
+        }
+
+        private void RollDie()
+        {
+            Random rand = new Random();
+            DiePictureBox.Image = Image.FromFile("die-faces.png");
+            switch (rand.Next(0, 6))
+            {
+                case 0:
+                    DiePictureBox.Image = cropImage(die, new Rectangle(0, 0, 128, 128));
+                    dierolls[0]++;
+                    break;
+                case 1:
+                    DiePictureBox.Image = cropImage(die, new Rectangle(128, 0, 128, 128));
+                    dierolls[1]++;
+                    break;
+                case 2:
+                    DiePictureBox.Image = cropImage(die, new Rectangle(256, 0, 128, 128));
+                    dierolls[2]++;
+                    break;
+                case 3:
+                    DiePictureBox.Image = cropImage(die, new Rectangle(384, 0, 128, 128));
+                    dierolls[3]++;
+                    break;
+                case 4:
+                    DiePictureBox.Image = cropImage(die, new Rectangle(512, 0, 128, 128));
+                    dierolls[4]++;
+                    break;
+                case 5:
+                    DiePictureBox.Image = cropImage(die, new Rectangle(640, 0, 128, 128));
+                    dierolls[5]++;
+                    break;
+            }
+            DiePictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            RollOutcomesLabel.Text = "1: " + dierolls[0] +
+                "\n2: " + dierolls[1] +
+                "\n3: " + dierolls[2] +
+                "\n4: " + dierolls[3] +
+                "\n5: " + dierolls[4] +
+                "\n6: " + dierolls[5];
         }
     }
 }
