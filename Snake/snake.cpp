332bbb377796d4159062e4ac4995c0be87b51abc
Ben Martin <bmartin6245@yahoo.com>
2024-06-18 17:33:28 -0600

    working changes

--- Snake/snake.cpp
+++ Snake/snake.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <algorithm> 
 #include <string>
 #include <thread>         // std::this_thread::sleep_for
 #include <chrono>         // std::chrono::seconds
@@ -26,15 +27,33 @@ class Snake {
             int col;
         } Position;
 
+        Position food;
         vector<Position> tiles;
         uint8_t last_direction;
         Snake() {
-            Position start;
-            start.row = 1;
-            start.col = 1;
+            Position start = {1, 1};
+            food = {1, 5};
             tiles.push_back(start);
             direction = RIGHT;
             last_direction = direction;
+        }
+
+        void generate_next_food() {
+            vector<Position> empty_spaces;
+            for (int row = 0; row < HEIGHT; row++) {
+                for (int col = 0; col < WIDTH; col++) {
+                    Position tile = {row, col};
+                    bool match = false;
+                    for (int i = 0; i < tiles.size(); i++) {
+                        if (tiles[i].row == row && tiles[i].col == col) {
+                            match = true;
+                            break;
+                        }
+                    }
+                    if (!match) empty_spaces.push_back(tile);
+                }
+            }
+            food = empty_spaces[rand() % empty_spaces.size()];
         }
 
         void travel() {
@@ -57,62 +76,68 @@ class Snake {
                 next = {tiles.back().row + 1, tiles.back().col};
             }
             tiles.push_back(next);
-            tiles.erase(tiles.begin());
+            if (next.row == food.row && next.col == food.col) {
+                generate_next_food();
+            } else {
+                tiles.erase(tiles.begin());
+            }
 
             last_direction = direction;
         }
 };
 
-void clear() {
-    COORD topLeft  = { 0, 0 };
-    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_SCREEN_BUFFER_INFO screen;
-    DWORD written;
+// void clear() {
+//     COORD topLeft  = { 0, 0 };
+//     HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+//     CONSOLE_SCREEN_BUFFER_INFO screen;
+//     DWORD written;
 
-    GetConsoleScreenBufferInfo(console, &screen);
-    FillConsoleOutputCharacterA(
-        console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
-    );
-    FillConsoleOutputAttribute(
-        console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
-        screen.dwSize.X * screen.dwSize.Y, topLeft, &written
-    );
-    SetConsoleCursorPosition(console, topLeft);
-}
+//     GetConsoleScreenBufferInfo(console, &screen);
+//     FillConsoleOutputCharacterA(
+//         console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+//     );
+//     FillConsoleOutputAttribute(
+//         console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+//         screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+//     );
+//     SetConsoleCursorPosition(console, topLeft);
+// }
 
 void draw_board() {
     Snake snake;
-
+    char board[HEIGHT][WIDTH];
     while (true) {
-        // clear();
-        char board[HEIGHT][WIDTH] = {
-            "___________________",
-            "___________________",
-            "___________________",
-            "___________________",
-            "___________________",
-            "___________________",
-            "___________________",
-            "___________________",
-            "___________________",
-            "___________________"
-        };
+        
+        // Whitespace padding (fix this by clearing console?)
         for (int i = 0; i < 50; i++) cout << endl;
-        // Replace snake tiles with x's
+
+        // Fill board with blanks
+        for (int row = 0; row < HEIGHT; row++) {
+            for (int col = 0; col < WIDTH; col++) {
+                board[row][col] = '_';
+            }
+        }
+
+        // Draw snake tiles
         for (int i = 0; i < snake.tiles.size(); i++) {
             board[snake.tiles[i].row][snake.tiles[i].col] = 'x';
         }
 
+        // Draw food
+        board[snake.food.row][snake.food.col] = 'o';
+
         // Print board
-        for (int i = 0; i < HEIGHT; i++) {
-            cout << board[i] << endl;
+        for (int row = 0; row < HEIGHT; row++) {
+            for (int col = 0; col < WIDTH; col++) {
+                cout << board[row][col];
+            }
+            cout << endl;
         }
-        cout << endl;
 
         // Move snake
         snake.travel();
 
-        this_thread::sleep_for (chrono::seconds(1));
+        this_thread::sleep_for(chrono::seconds(1));
     }
 }
 
